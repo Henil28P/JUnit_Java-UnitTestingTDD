73f87f04b85e4ccb8248987fe12a34198ff27200
Henil28P <henilp467@gmail.com>
2025-01-20 18:55:49 +1100

    create and add relevant setup files for JUnit testing project

---
+++ src/main/java/org/example/junit_unittesting_tdd/App.java
@@ -0,0 +1,10 @@
+package org.example.junit_unittesting_tdd;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
