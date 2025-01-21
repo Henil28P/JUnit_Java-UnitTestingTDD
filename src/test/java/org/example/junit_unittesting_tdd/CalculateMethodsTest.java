005cc720cc6a0c1bee1130974157ac06e9366fe6
Henil28P <henilp467@gmail.com>
2025-01-21 13:09:56 +1100

    [CalculateMethodsTest.java]: create a test method using @Test annotation for testing expected result of divide() in calculateMethods instance

--- src/test/java/org/example/junit_unittesting_tdd/CalculateMethodsTest.java
+++ src/test/java/org/example/junit_unittesting_tdd/CalculateMethodsTest.java
@@ -1,5 +1,15 @@
 package org.example.junit_unittesting_tdd;
 
-public class CalculateMethodsTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+public class CalculateMethodsTest {
+	private CalculateMethods calculateMethods = new CalculateMethods(); // create a private property of type CalculateMethods and create an instance of it
+
+	// Function to test divide() method in calculateMethods instance
+	@Test
+	public void testDivide()
+	{
+		Assertions.assertEquals(10, calculateMethods.divide(100,10)); // assertEquals(expectedResult, function to call with parameters)
+	}
 }
