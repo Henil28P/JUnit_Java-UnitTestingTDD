b2f56416c0cc0bb29ce7d2dc902766ab70556907
Henil28P <henilp467@gmail.com>
2025-01-21 13:40:35 +1100

    [Code.java]: create new Java class for returning simple 'Hello world!' string

---
+++ src/main/java/org/example/junit_unittesting_tdd/Code.java
@@ -0,0 +1,9 @@
+package org.example.junit_unittesting_tdd;
+
+public class Code {
+	// Function to simply return the text "Hello world!"
+	public String sayHello()
+	{
+		return "Hello world!";
+	}
+}
