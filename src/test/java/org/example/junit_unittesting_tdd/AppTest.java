cc90940442efeeaab91bd9bb7c5d2ba2ba38c5f8
Henil28P <henilp467@gmail.com>
2025-01-21 11:13:00 +1100

    [AppTest.java]: import jupiter-api Assertions class and methods for JUnit assertions at basic example test

--- src/test/java/org/example/junit_unittesting_tdd/AppTest.java
+++ src/test/java/org/example/junit_unittesting_tdd/AppTest.java
@@ -1,6 +1,7 @@
 package org.example.junit_unittesting_tdd;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
 
 import org.junit.jupiter.api.Test;
 
