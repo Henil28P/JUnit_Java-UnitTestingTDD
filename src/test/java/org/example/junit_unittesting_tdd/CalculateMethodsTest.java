ad32193b4c20bdec275cd5f5b232116f34a6f2fc
Henil28P <henilp467@gmail.com>
2025-01-21 13:33:20 +1100

    [CalculateMethodsTest.java]: make Assertions class 'static' and remove Assertions in front of every assertX() methods used

--- src/test/java/org/example/junit_unittesting_tdd/CalculateMethodsTest.java
+++ src/test/java/org/example/junit_unittesting_tdd/CalculateMethodsTest.java
@@ -1,6 +1,6 @@
 package org.example.junit_unittesting_tdd;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -18,14 +18,14 @@ public class CalculateMethodsTest {
 	@Test
 	public void testDivide()
 	{
-		Assertions.assertEquals(10, calculateMethods.divide(100,10)); // assertEquals(expectedResult, function to call with parameters)
+		assertEquals(10, calculateMethods.divide(100,10)); // assertEquals(expectedResult, function to call with parameters)
 	}
 
 	// Test for an exception to occur (eg. when dividing by 0)
 	@Test
 	public void testDivideByZero()
 	{
-		Assertions.assertThrows(ArithmeticException.class, () -> calculateMethods.divide(100, 0));
+		assertThrows(ArithmeticException.class, () -> calculateMethods.divide(100, 0));
 		// assertThrows(<typeOfException>, Executable to implement using Lambda expression that takes no parameter but calls the divide() function)
 	}
 }
