bfbc036794b9c7da973dc1c51b74ef093d2d6daf
Henil28P <henilp467@gmail.com>
2025-01-21 15:35:29 +1100

    [BankAccountTests.java]: add @DisplayName annotation above both test methods and test class to use custom names over default names in test report

--- src/test/java/org/example/junit_unittesting_tdd/basic_unit_tests/BankAccountTests.java
+++ src/test/java/org/example/junit_unittesting_tdd/basic_unit_tests/BankAccountTests.java
@@ -3,22 +3,26 @@ package org.example.junit_unittesting_tdd.basic_unit_tests;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.example.junit_unittesting_tdd.basic_junit_tests.BankAccount;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Test BankAccount class")
 public class BankAccountTests {
 	// Test withdraw operation
 	@Test
+	@DisplayName("Withdraw 500 successfully")
 	public void testWithdraw()
 	{
 		BankAccount bankAccount = new BankAccount(500, -1000); // create a new instance of BankAccount class with starter values of balance as 500 and minimumBalance as -1000
 		
 		// withdraw 300 and use assertEquals to check expected result of withdraw operation functionality
 		bankAccount.withdraw(300);
-		assertEquals(250, bankAccount.getBalance()); // check if 500(current balance)-300(withdrew)=200(new balance)
+		assertEquals(200, bankAccount.getBalance()); // check if 500(current balance)-300(withdrew)=200(new balance)
 	}
 
 	// Test deposition operation
 	@Test
+	@DisplayName("Deposit 400 successfully")
 	public void testDeposit()
 	{
 		BankAccount bankAccount = new BankAccount(400,0); // initial balance as 400 and minimumBalance as 0
