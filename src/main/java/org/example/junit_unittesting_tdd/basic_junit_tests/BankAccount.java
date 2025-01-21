66c15d52e0706eb3ce53834b69c64f5cf1b3e3b4
Henil28P <henilp467@gmail.com>
2025-01-21 14:57:50 +1100

    [BankAccount.java]: add withdraw() method to BankAccount class that returns withdrawing amount

--- src/main/java/org/example/junit_unittesting_tdd/basic_junit_tests/BankAccount.java
+++ src/main/java/org/example/junit_unittesting_tdd/basic_junit_tests/BankAccount.java
@@ -19,5 +19,20 @@ public class BankAccount {
 
 	public double getMinimumBalance() {
 		return minimumBalance;
-	}	
+	}
+
+	// create 2 methods for bank operations (withdraw and deposit)
+	// withdraw() will return the amount that we are withdrawing, and if not possible, then throw a runtime exception
+	public double withdraw(double amount)
+	{
+		if (balance - amount > minimumBalance)
+		{
+			balance -= amount; // replace the balance with new balance after withdrawal
+			return amount;
+		}
+		else
+		{
+			throw new RuntimeException();
+		}
+	}
 }
