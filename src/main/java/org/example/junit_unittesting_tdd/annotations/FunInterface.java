7e32fcbecd0d9b1c800715c67d92aa0df96dddee
Henil28P <henilp467@gmail.com>
2025-01-21 14:39:02 +1100

    [FunInterface.java]: use @FunctionalInterface in Java Interface to experiment

---
+++ src/main/java/org/example/junit_unittesting_tdd/annotations/FunInterface.java
@@ -0,0 +1,6 @@
+package org.example.junit_unittesting_tdd.annotations;
+
+@FunctionalInterface // this will make sure only 1 abstract method is allowed in this interface
+public interface FunInterface {
+	void function();
+}
