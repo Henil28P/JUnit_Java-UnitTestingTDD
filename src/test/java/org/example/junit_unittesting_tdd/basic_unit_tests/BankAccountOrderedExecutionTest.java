ade00513db0397ce81d281a8075d0db61054729c
Henil28P <henilp467@gmail.com>
2025-01-26 18:58:17 +1100

    [BankAccountOrderedExecutionTest.java]: create a new Java test file for ordered execution of withdrawal and deposit operations

---
+++ src/test/java/org/example/junit_unittesting_tdd/basic_unit_tests/BankAccountOrderedExecutionTest.java
@@ -0,0 +1,27 @@
+package org.example.junit_unittesting_tdd.basic_unit_tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.example.junit_unittesting_tdd.basic_junit_tests.BankAccount;
+import org.junit.jupiter.api.Test;
+
+public class BankAccountOrderedExecutionTest {
+	// This class will contain 2 methods: testWithdraw() and testDeposit()
+	static BankAccount bankAccount = new BankAccount(0,0); // instead of creating new instance in every test methods, let's give this one a 'static' bank account instead
+	
+	@Test
+	public void testWithdraw()
+	{
+		// Throw an exception whenever wanting to withdraw more than the minimum balance
+		// eg. when min balance is 0, if our withdraw gets called before our deposit, we will get an exception - order matters
+		bankAccount.withdraw(300);
+		assertEquals(200,bankAccount.getBalance());
+	}
+
+	@Test
+	public void testDeposit()
+	{
+		bankAccount.deposit(500);
+		assertEquals(500,bankAccount.getBalance());
+	}
+}
