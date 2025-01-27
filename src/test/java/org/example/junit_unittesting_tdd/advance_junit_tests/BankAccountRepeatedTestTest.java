4b3a1ff7f5f906013a79a8b639764969c2c8d486
Henil28P <henilp467@gmail.com>
2025-01-27 14:19:39 +1100

    [BankAccountRepeatedTestTest.java]: implement new method to use repetitionInfo parameter and output with .getCurrentRepetition() to output current repetition

--- src/test/java/org/example/junit_unittesting_tdd/advance_junit_tests/BankAccountRepeatedTestTest.java
+++ src/test/java/org/example/junit_unittesting_tdd/advance_junit_tests/BankAccountRepeatedTestTest.java
@@ -5,17 +5,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.example.junit_unittesting_tdd.basic_junit_tests.BankAccount;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepetitionInfo;
+//import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(BankAccountParameterResolver.class)
 public class BankAccountRepeatedTestTest {
 	// Test deposition operation
 	@RepeatedTest(5) // this test will now run 5 times (replace @Test with @RepeatedTest(n) to run the test 'n' times)
-	@DisplayName("Deposit 400 successfully")
+	@DisplayName("Deposit 500 successfully")
 	public void testDeposit(BankAccount bankAccount)
 	{
 		bankAccount.deposit(500); // deposit 500 to current balance of 400 to bankAccount
 		assertEquals(500,bankAccount.getBalance()); // check if 400(current balance) + 500(deposited amount) = 900 (new balance)
+	}
+
+	@RepeatedTest(5)
+	@DisplayName("Deposit 500 successfully")
+	public void testDepositRepetitionInfo(BankAccount bankAccount, RepetitionInfo repetitionInfo)
+	{
+		bankAccount.deposit(500); // deposit 500 to current balance of 400 to bankAccount
+		assertEquals(500,bankAccount.getBalance()); // check if 400(current balance) + 500(deposited amount) = 900 (new balance)
+		System.out.println("Number: " + repetitionInfo.getCurrentRepetition()); // to output the current repetition
 	}
 }
