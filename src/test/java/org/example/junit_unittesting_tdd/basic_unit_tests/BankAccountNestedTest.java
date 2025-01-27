93401109b733b601ba8d561bfe17f7479a6005ee
Henil28P <henilp467@gmail.com>
2025-01-27 11:45:26 +1100

    [BankAccountNestedTest.java]: add an inner class for when balance = 0 and use @Nested to run those tests in the child class

--- src/test/java/org/example/junit_unittesting_tdd/basic_unit_tests/BankAccountNestedTest.java
+++ src/test/java/org/example/junit_unittesting_tdd/basic_unit_tests/BankAccountNestedTest.java
@@ -1,9 +1,11 @@
 package org.example.junit_unittesting_tdd.basic_unit_tests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.example.junit_unittesting_tdd.basic_junit_tests.BankAccount;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class BankAccountNestedTest {
@@ -28,5 +30,32 @@ public class BankAccountNestedTest {
 
 		bankAccount.deposit(500); // deposit 500 to current balance of 400 to bankAccount
 		assertEquals(900,bankAccount.getBalance()); // check if 400(current balance) + 500(deposited amount) = 900 (new balance)
+	}
+
+	// Create an inner class for when balance = 0
+	@Nested
+	class WhenBalanceEqualsZero
+	{
+		// Note that when this test file is run, only the 2 methods in the outer class will run and not the ones in this WhenBalanceEqualsZero class
+		// This is because we have to add @Nested in order for these to get run as well
+		// Also note that running test of a parent class will also execute all of the tests of her child's class (when we add @Nested on the class level - above the class name)
+		@Test
+		@DisplayName("Withdrawing below minimum balance exception")
+		public void testWithdrawMinimumBalanceIs0()
+		{
+			BankAccount bankAccount = new BankAccount(0,0); // create a BankAccount instance
+			// In this case, we expect an exception
+			assertThrows(RuntimeException.class, () -> bankAccount.withdraw(500)); // assertThrows because we expect an exception to be thrown
+		}
+
+		@Test
+		@DisplayName("Withdrawing and getting a negative balance")
+		public void testWithdrawMinimumBalanceNegative1000()
+		{
+			BankAccount bankAccount = new BankAccount(0,-1000);
+			bankAccount.withdraw(500);
+			// In this case, we get a new balance of -500
+			assertEquals(-500, bankAccount.getBalance());
+		}
 	}
 }
