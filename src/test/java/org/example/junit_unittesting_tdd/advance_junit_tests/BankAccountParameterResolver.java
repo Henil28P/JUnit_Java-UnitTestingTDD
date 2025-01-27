083853269410bb364fec0140440c381cbc48fc25
Henil28P <henilp467@gmail.com>
2025-01-27 13:50:56 +1100

    [BankAccountParameterResolver.java]: implement ParameterResolver interface and implement supportsParameter() and resolveParameter() methods to hand object

--- src/test/java/org/example/junit_unittesting_tdd/advance_junit_tests/BankAccountParameterResolver.java
+++ src/test/java/org/example/junit_unittesting_tdd/advance_junit_tests/BankAccountParameterResolver.java
@@ -1,5 +1,28 @@
 package org.example.junit_unittesting_tdd.advance_junit_tests;
 
-public class BankAccountParameterResolver {
+import org.example.junit_unittesting_tdd.basic_junit_tests.BankAccount;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
 
+public class BankAccountParameterResolver implements ParameterResolver {
+
+	@Override
+	public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
+			throws ParameterResolutionException {
+		// TODO Auto-generated method stub
+		return parameterContext.getParameter().getType() == BankAccount.class; // this is supported as soon as we're talking about a BankAccount instance
+		// if the type is of type BankAccount class, it should return true
+	}
+
+	// resolveParameter() will hand the object to us
+	@Override
+	public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
+			throws ParameterResolutionException {
+		// TODO Auto-generated method stub
+		// make sure resolveParameter() is not resolving the object to null as we don't need all the null pointers in our code
+		return new BankAccount(0,0); // return a new bank account
+	}
+	// Now we can use this dependency in our test and no need to create new instances of BankAccount in there	
 }
