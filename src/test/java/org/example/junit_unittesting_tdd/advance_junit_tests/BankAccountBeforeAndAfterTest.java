d608ccba4e77c215b76ffd6a2d8de3e6f8537b6d
Henil28P <henilp467@gmail.com>
2025-01-28 17:06:06 +1100

    [BankAccountBeforeAndAfterTest.java]: use @BeforeAll to initiate bankAccount object to run before all tests at once

--- src/test/java/org/example/junit_unittesting_tdd/advance_junit_tests/BankAccountBeforeAndAfterTest.java
+++ src/test/java/org/example/junit_unittesting_tdd/advance_junit_tests/BankAccountBeforeAndAfterTest.java
@@ -3,26 +3,35 @@ package org.example.junit_unittesting_tdd.advance_junit_tests;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.example.junit_unittesting_tdd.basic_junit_tests.BankAccount;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 public class BankAccountBeforeAndAfterTest {
 	// This class will contain 2 methods: testWithdraw() and testDeposit()
-		static BankAccount bankAccount = new BankAccount(500,0); // instead of creating new instance in every test methods, let's give this one a 'static' bank account instead
-		
-		@Test
-		public void testWithdraw()
-		{
-			// Throw an exception whenever wanting to withdraw more than the minimum balance
-			// eg. when min balance is 0, if our withdraw gets called before our deposit, we will get an exception - order matters
-			bankAccount.withdraw(300);
-			assertEquals(200,bankAccount.getBalance());
-		}
+	static BankAccount bankAccount;
 
-		@Test
-		public void testDeposit()
-		{
-			bankAccount.deposit(500);
-			assertEquals(700,bankAccount.getBalance());
-		}
+	@BeforeAll // To run code in prepTest() before all other test methods
+	public static void prepTest()
+	{
+		// Initiate the bankAccount in this method instead of top to run before all other test methods
+		bankAccount = new BankAccount(500,0); // instead of creating new instance in every test methods, let's give this one a 'static' bank account instead
+	}
+
+	// Hence, the bankAccount will get modified by the 1st method (above) and so with the 2nd as it's the same object
+	@Test
+	public void testWithdraw()
+	{
+		// Throw an exception whenever wanting to withdraw more than the minimum balance
+		// eg. when min balance is 0, if our withdraw gets called before our deposit, we will get an exception - order matters
+		bankAccount.withdraw(300);
+		assertEquals(200,bankAccount.getBalance());
+	}
+
+	@Test
+	public void testDeposit()
+	{
+		bankAccount.deposit(500);
+		assertEquals(700,bankAccount.getBalance());
+	}
 }
