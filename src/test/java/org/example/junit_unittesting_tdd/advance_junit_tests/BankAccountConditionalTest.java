03ab509696f41dc06d1674e3713478b51a96c849
Henil28P <henilp467@gmail.com>
2025-01-28 18:52:29 +1100

    [BankAccountConditionalTest.java]: use @EnabledOnJre() and @DisabledOnJre() to run test methods on specific JRE versions

--- src/test/java/org/example/junit_unittesting_tdd/advance_junit_tests/BankAccountConditionalTest.java
+++ src/test/java/org/example/junit_unittesting_tdd/advance_junit_tests/BankAccountConditionalTest.java
@@ -1,7 +1,10 @@
 package org.example.junit_unittesting_tdd.advance_junit_tests;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnJre;
+import org.junit.jupiter.api.condition.EnabledOnJre;
 import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.JRE;
 import org.junit.jupiter.api.condition.OS;
 
 public class BankAccountConditionalTest {
@@ -21,4 +24,18 @@ public class BankAccountConditionalTest {
 		;
 	}
 	// from the above tests, any 1 of them will be run and pass based on the OS the user runs on
+
+	@Test
+	@EnabledOnJre({JRE.JAVA_17}) // test should only be allowed on JRE Java16 (can specify multiple versions in the array)
+	public void testJRE()
+	{
+		;
+	}
+
+	@Test
+	@DisabledOnJre({JRE.JAVA_8}) // to allow tests to run on JRE except the ones specified in this array of @DisabledOnJre annotation and ignore all tests that use these JRE in this list
+	public void testNoJRE8()
+	{
+		;
+	}
 }
