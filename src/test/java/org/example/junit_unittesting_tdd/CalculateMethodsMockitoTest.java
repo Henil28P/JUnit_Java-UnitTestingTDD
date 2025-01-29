3a084afa6b9dd360144d4b50123948f05f1755c9
Henil28P <henilp467@gmail.com>
2025-01-29 22:17:13 +1100

    [CalculateMethodsMockitoTest.java]: set up mock method and use Mockito.when() to specify what needs to happen when doing a certain divide action

--- src/test/java/org/example/junit_unittesting_tdd/CalculateMethodsMockitoTest.java
+++ src/test/java/org/example/junit_unittesting_tdd/CalculateMethodsMockitoTest.java
@@ -10,4 +10,16 @@ import org.mockito.junit.jupiter.MockitoExtension;
 public class CalculateMethodsMockitoTest {
 	@Mock // no need to initialise the object
 	CalculateMethods calculateMethods; //  object
+
+	// define how our Mock will behave - it will override functionality
+	// Hence, we cannot use price, fields and methods just like this - this is where we need Bio Mock or write our own reflection code
+	// For now, it's possible to test private methods
+
+	// Specify what needs to happen when we call our defined methods - to do this, create a method that should run before the tests
+	@BeforeEach
+	public void setupMock()
+	{
+		// in the Mockito.when(<specify what needs to happen when doing a certain action>)
+		Mockito.when(calculateMethods.divide(6, 3)).thenReturn(2.0); // whenever the divide() is called with parameters 6 and 3, we will return 2.0
+	}
 }
